74bec51def18ee1ce65a1f084260167c9b4863f0
Sofia Tseitlin <sofi.tseitlin@yandex.com>
2025-01-07 12:25:20 +0300

    try to improve fail with 'POSTS'

--- tests/test_views.py
+++ tests/test_views.py
@@ -3,7 +3,7 @@ def test_blog_posts(posts):
         from blog.views import POSTS as solution_posts
     except Exception as e:
         raise AssertionError(
-            'При импорте списка `posts` из файла `blog/views.py` '
+            'При импорте списка `POSTS` из файла `blog/views.py` '
             f'произошла ошибка: {e}') from e
     assert solution_posts == posts, (
         'Убедитесь, что список с постами `posts` из файла `blog/views.py` '
