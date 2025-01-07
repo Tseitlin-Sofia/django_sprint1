263a90ca366e02ead5753026511e296dc8a584d5
Sofia Tseitlin <sofi.tseitlin@yandex.com>
2025-01-08 01:06:40 +0300

    improving tests/test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,5 +6,5 @@ def test_blog_posts(posts):
             'При импорте списка `POSTS` из файла `blog/views.py` '
             f'произошла ошибка: {e}') from e
     assert solution_posts == posts, (
-        'Убедитесь, что список с постами `posts` из файла `blog/views.py` '
+        'Убедитесь, что список с постами `POSTS` из файла `blog/views.py` '
         'соответствуют списку из задания.')
